05e9025cb4ae197c19ac7b55a109aecfa90218b1
Florian Pobbig <florian.pobbig@gmail.com>
2017-12-20 22:34:12 +0100

    moved star section

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ app.get("/", function(req, res){
     res.send("Bonjour");
 });
 
-app.get("*", function(req, res){
-    res.send("you are a star");
-})
-
 app.get("/blop", function(req, res){
     res.send("Bonbye");
 });
 
+app.get("*", function(req, res){
+    res.send("you are a star");
+})
 app.listen(port, function(){
     console.log(`Server listening on port ${port}`);
 })
